3d10a1315b5fb5124618204128fb0021fc6f44e0
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-03-21 23:36:23 +0530

    Apply get a User Function

--- controller/userController.js
+++ controller/userController.js
@@ -7,22 +7,22 @@ const createUser = asyncHandler(async (req, res) => {
   // console.log(req.body);
   const email = req.body.email;
   // console.log(email);
-  const findUser = await User.findOne({email: email});
+  const findUser = await User.findOne({ email: email });
   if (!findUser) {
     // create user
     const newUser = await User.create(req.body);
     res.json(newUser);
-  }else {
+  } else {
     // user already exists
     res.json({
       message: "User Already Exists",
-      success: false
+      success: false,
     });
   }
 });
 
 // get all users
-const getAllUsers = asyncHandler(async(req, res)=>{
+const getAllUsers = asyncHandler(async (req, res) => {
   // console.log("get all users");
   try {
     const allUsers = await User.find();
@@ -30,6 +30,19 @@ const getAllUsers = asyncHandler(async(req, res)=>{
   } catch (error) {
     console.log(error);
   }
-})
+});
 
-export { createUser, getAllUsers };
+// get a user
+const getAUser = asyncHandler(async (req, res) => {
+  // console.log("get a user");
+  const { _id } = req.params;
+  // console.log(_id);
+  try {
+    const foundUser = await User.findById(_id);
+    res.json(foundUser);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export { createUser, getAllUsers, getAUser };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createUser, getAllUsers } from "../controller/userController.js";
+import { createUser, getAUser, getAllUsers } from "../controller/userController.js";
 
 const router = express.Router();
 
 router.post("/create-user", createUser);
 router.get("/get-all-users", getAllUsers);
+router.get("/get-a-user/:_id", getAUser);
 
 export default router;
